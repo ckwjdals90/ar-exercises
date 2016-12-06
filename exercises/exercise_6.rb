736c0fee9da5345d58b5048f5e00b44f97afb7d1
Jungmin Cha <ckwjdals90@gmail.com>
2016-12-06 22:44:30 +0000

    exercise 6 refactored

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,10 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create( first_name: 'Jungmin', last_name: 'Cha', hourly_rate: 99 )
-@store1.employees.create( first_name: 'Junsung', last_name: 'Park', hourly_rate: 75 )
-@store1.employees.create( first_name: 'Hosong', last_name: 'Lim', hourly_rate: 75 )
+@store1.employees.create( first_name: 'AAA', last_name: 'A', hourly_rate: 99 )
+@store1.employees.create( first_name: 'BBB', last_name: 'B', hourly_rate: 75 )
+@store1.employees.create( first_name: 'CCC', last_name: 'C', hourly_rate: 75 )
 
-@store2.employees.create( first_name: 'Sunyoung', last_name: 'Park', hourly_rate: 90 )
-@store2.employees.create( first_name: 'Julie', last_name: 'Park', hourly_rate: 75 )
-@store2.employees.create( first_name: 'Sharon', last_name: 'Jun', hourly_rate: 75 )
+@store2.employees.create( first_name: 'DDD', last_name: 'D', hourly_rate: 90 )
+@store2.employees.create( first_name: 'EEE', last_name: 'E', hourly_rate: 75 )
+@store2.employees.create( first_name: 'FFF', last_name: 'F', hourly_rate: 75 )
